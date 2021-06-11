c75ab754a9f67ea1c158d803b443ef399304001b
Daniel Avelino <daniel_avelino@outlook.com>
2021-06-11 15:56:57 -0300

    adjust localdate

--- OrangeTalents/src/main/java/com/zup/orangeTalents/factory/UserFactory.java
+++ OrangeTalents/src/main/java/com/zup/orangeTalents/factory/UserFactory.java
@@ -1,13 +1,5 @@
 package com.zup.orangeTalents.factory;
 
-import com.zup.orangeTalents.DTO.UserDTO;
-import com.zup.orangeTalents.entities.User;
-
 public class UserFactory {
 
-	public UserDTO newUserDTO(User user) {
-		UserDTO dto = new UserDTO(user.getId(), user.getName(), user.getEmail(), user.getCpf(), user.getBirthday(),
-				user.getCars());
-		return dto;
-	}
 }
